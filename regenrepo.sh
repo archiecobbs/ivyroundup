b46b8c81dcd4d2f704bb8a3d8fe6c8da662547fa
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2009-01-16 16:52:37 +0000

    Add some more logic and checks.

--- regenrepo.sh
+++ regenrepo.sh
@@ -8,13 +8,36 @@
 # Bail on errors, show commands
 set -e
 
+# There should be no outstanding changes
+echo 'regenrepo: checking status of source'
+if ! [ "`svn st src | wc -c`" -eq 0 ]; then
+    echo 'regenrepo: ERROR: src/ is not clean:'
+    svn st src
+    exit 1
+fi
+
 # Update to get lastest stuff
 echo 'regenrepo: updating from SVN'
 svn up
 
 # Regenerate repo
 echo 'regenrepo: regenerating repository'
-ant repo
+ant repo > regen.out
+
+# Check for errors (fatal)
+if grep -q ERROR regen.out; then
+    echo 'regenrepo: ERROR: errors found during repository generation:'
+    grep ERROR regen.out
+    rm -f regen.out
+    exit 1
+fi
+
+# Check for warnings (non-fatal)
+if grep -q WARNING regen.out; then
+    echo 'regenrepo: warnings found during repository generation:'
+    grep WARNING regen.out
+fi
+rm -f regen.out
 
 # If there's anything new, svn copy it from source
 if svn st repo/modules | grep -q '^\?'; then
