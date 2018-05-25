fb15e307b2ec3974a76dc6ad8f4663493bc99863
Archie L. Cobbs <archie.cobbs@gmail.com>
2018-05-25 18:28:50 -0500

    Fix bugs in a2767539 and 2eb5cd3b.

--- verify-changes.sh
+++ verify-changes.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-MOD_ORGS`git status | grep '(new file|modified):' | awk -F/ '{print $3}' | sort -u | xargs printf '%s,' | sed 's/,$//g'`
-MOD_MODS`git status | grep '(new file|modified):' | awk -F/ '{print $4}' | sort -u | xargs printf '%s,' | sed 's/,$//g'`
-MOD_REVS`git status | grep '(new file|modified):' | awk -F/ '{print $5}' | sort -u | xargs printf '%s,' | sed 's/,$//g'`
+set -e
+
+# Clean up repo
+git checkout repo
+git clean -fd repo
+
+# Determine what's changed
+MOD_ORGS=`git status | grep -E '(new file|modified):[[:space:]]*src/modules/' | awk -F/ '{print $3}' | sort -u | xargs printf '%s,' | sed 's/,$//g'`
+MOD_MODS=`git status | grep -E '(new file|modified):[[:space:]]*src/modules/' | awk -F/ '{print $4}' | sort -u | xargs printf '%s,' | sed 's/,$//g'`
+MOD_REVS=`git status | grep -E '(new file|modified):[[:space:]]*src/modules/' | awk -F/ '{print $5}' | sort -u | xargs printf '%s,' | sed 's/,$//g'`
+
+# Resolve modified modules
+printf '\nVerifying:\n  orgs: %s\n  mods: %s\n  revs: %s\n\n' "${MOD_ORGS}" "${MOD_MODS}" "${MOD_REVS}"
 
 ant \
   -Dresolve.org="${MOD_ORGS}" \
